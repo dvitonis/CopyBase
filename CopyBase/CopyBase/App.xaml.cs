b9987bbd17a1ab5dd03eae9046d5ad6886d82a8a
Darius Vitonis <flowupon@gmail.com>
2013-11-06 00:02:11 +0100

    Exception logging

--- CopyBase/CopyBase/App.xaml.cs
+++ CopyBase/CopyBase/App.xaml.cs
@@ -12,5 +12,23 @@ namespace CopyBase
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+        }
+
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"Errorlog.txt", true))
+            {
+                file.WriteLine(DateTime.Now + "\t" + e.ToString());
+            }
+
+            if (args.IsTerminating)
+                System.Windows.Forms.MessageBox.Show("Runtime terminating \n\n" + e.Message);
+        }
     }
 }
